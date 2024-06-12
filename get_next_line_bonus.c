604f13f77a4844b29a68a037591f82b1f9ad2757
Eunsu Ahn <theeunsu@gmail.com>
2024-06-12 17:37:43 +0200

    Add files via upload

---
+++ get_next_line_bonus.c
@@ -0,0 +1,178 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eahn <eahn@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/22 22:42:09 by eahn              #+#    #+#             */
+/*   Updated: 2024/03/27 15:35:49 by eahn             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+// MAX_FD is defined in the header file as 10240
+// the number was found using 'ulimit -a'
+// fd number cannot be modifed more than 10240
+
+char	*get_next_line(int fd)
+{
+	char		*line;
+	static char	*static_str[MAX_FD];
+	int			newline_len;
+
+	if (fd < 0 || BUFFER_SIZE <= 0 || fd >= MAX_FD)
+		return (NULL);
+	static_str[fd] = read_save_str(fd, static_str[fd]);
+	if (!static_str[fd])
+		return (NULL);
+	line = extract_line(static_str[fd], &newline_len);
+	static_str[fd] = extract_next_str(static_str[fd], &newline_len);
+	return (line);
+}
+
+//*DESCRIPTION
+// Reads datas from a file descriptor and stores it until encountering '\n'
+// The read content is appended to the dynamically allocated string 'static_str'
+//*PARAMETERS
+//#1 int fd
+//#2 char *static_str: a pointer to static_str holding previously read content
+//*RETURN VALUES
+//#static _str: a pointer to static_str containing the concatenated str
+char	*read_save_str(int fd, char *static_str)
+{
+	char	*buffer;
+	int		read_bytes;
+
+	buffer = gnl_calloc((BUFFER_SIZE + 1), sizeof(char));
+	if (!buffer)
+		return (NULL);
+	while (!gnl_strchr(static_str, '\n'))
+	{
+		read_bytes = read(fd, buffer, BUFFER_SIZE);
+		if (read_bytes == -1)
+		{
+			free(buffer);
+			free(static_str);
+			buffer = NULL;
+			static_str = NULL;
+			return (NULL);
+		}
+		if (read_bytes == 0)
+			break ;
+		buffer[read_bytes] = '\0';
+		static_str = gnl_strjoin(static_str, buffer);
+	}
+	free(buffer);
+	buffer = NULL;
+	return (static_str);
+}
+
+// *DESCRIPTION
+// Extracts a singlie line from 'static_str'.
+// Allocates and returns the substring(line) until encountering a '\n'.
+// *PARAMETERS
+// #1 char *static_str: a pointer to a string
+// from which a line will be extracted
+// #2 int *newline_len: a pointer to an int
+// where the length of the extracted line will be stored
+// *RETURN VALUES
+// #line : a pointer to a dynamically allocated string
+// containing the extracted line
+char	*extract_line(char *static_str, int *newline_len)
+{
+	char	*line;
+
+	if (!static_str)
+		return (NULL);
+	*newline_len = gnl_strchr(static_str, '\n');
+	if (*newline_len == 0)
+		*newline_len = gnl_strlen(static_str);
+	if (*newline_len == 0)
+	{
+		free(static_str);
+		static_str = NULL;
+		return (NULL);
+	}
+	line = gnl_calloc((*newline_len + 1), sizeof(char));
+	if (!line)
+		return (NULL);
+	gnl_strcpy(line, static_str, *newline_len + 1);
+	return (line);
+}
+
+// *DESCRIPTION
+// Returns the remaining content of 'static_str' after extracting a line.
+// It essentially returns the content after the line extracted by 'extract_line'
+// *PARAMETERS
+// #1 char *static_str: a pointer to a string
+// from which the next line after the extracted line will be retrieved
+// #2 int *newline_len: an integer representing the length of the extracted line
+// *RETURN VALUES
+// #next _str: a pointer to a dynamically allocated string
+// containing the remaining content after the extracted line
+// #releases the memory allocated for 'static_str'.
+char	*extract_next_str(char *static_str, int *newline_len)
+{
+	char	*next_str;
+	int		size;
+
+	if (!static_str)
+		return (NULL);
+	size = gnl_strlen(static_str) - *newline_len + 1;
+	if (size == 1)
+	{
+		free(static_str);
+		return (NULL);
+	}
+	next_str = gnl_calloc(size, sizeof(char));
+	if (!next_str)
+		return (NULL);
+	gnl_strcpy(next_str, static_str + *newline_len, size);
+	free(static_str);
+	static_str = NULL;
+	return (next_str);
+}
+
+// int	main(void)
+// {
+// 	char	*result_line;
+// 	int		nb_line;
+// 	int		fd1, fd2, fd3;
+
+// 	nb_line = 1;
+// 	fd1 = open("./test.txt", O_RDONLY);
+// 	fd2 = open("./test2.txt", O_RDONLY);
+// 	fd3 = open("./test3.txt", O_RDONLY);
+// // printf("FD : %d\n", fd);
+// 	while (nb_line)
+// 	{
+// 		result_line = get_next_line(fd1);
+// 		if (result_line == NULL)
+// 			break ;
+// 		printf("line %d: %s\n", nb_line, result_line);
+// 		nb_line++;
+// 		free(result_line);
+
+// 		result_line = get_next_line(fd2);
+// 		if (result_line == NULL)
+// 			break ;
+// 		printf("line %d: %s\n", nb_line, result_line);
+// 		nb_line++;
+// 		free(result_line);
+
+// 		result_line = get_next_line(fd3);
+// 		if (result_line == NULL)
+// 			break ;
+// 		printf("line %d: %s\n", nb_line, result_line);
+// 		nb_line++;
+// 		free(result_line);
+
+// 	}
+// 	// system("leaks --list -- a.out");
+// 	close(fd1);
+// 	close(fd2);
+// 	close(fd3);
+// 	return (0);
+// }
---
+++ get_next_line_bonus.h
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eahn <eahn@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/18 10:38:19 by eahn              #+#    #+#             */
+/*   Updated: 2024/03/27 15:37:39 by eahn             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# define MAX_FD 10240
+
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 5
+# endif
+
+/* get_next_line.c */
+char	*get_next_line(int fd);
+char	*read_save_str(int fd, char *static_str);
+char	*extract_line(char *static_str, int *newline_len);
+char	*extract_next_str(char *static_str, int *newline_len);
+
+/* get_next_line_utils.c */
+int		gnl_strlen(const char *s);
+char	*gnl_strjoin(char *s1, char *s2);
+int		gnl_strchr(char *s, int c);
+char	*gnl_strcpy(char *dst, char *src, size_t n);
+char	*gnl_calloc(size_t count, size_t size);
+
+#endif
